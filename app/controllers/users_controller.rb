c8a88921b520907ca5295ac8da64b075c73d60ac
suzukisan22 <getty2017@gmail.com>
2015-08-16 01:53:31 +0000

    update and edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,11 +19,27 @@ def create
       render 'new'
     end
   end
+  
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+   def update
+     @user = User.find(params[:id])
+    if @user.update(user_params)
+      #保存に成功した場合はトップページへダイレクト
+      flash[:success] = "Edit succeed"
+      redirect_to :action => "show"
+    else
+      #保存に失敗した場合は編集画面へ戻す
+      render 'edit'
+    end
+  end
 
   private
 
   def user_params
     params.require(:user).permit(:name, :email, :password,
-                                 :password_confirmation)
+                                 :password_confirmation, :address)
   end
 end
